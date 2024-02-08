c4915a859689206572262b519272c12b12e70faa
Anish0369 <anish231003@gmail.com>
2024-02-09 00:39:44 +0530

    "Updated"

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,54 @@
 "use strict";
-console.log('Hello, world!');
+// console.log('Hello, world!');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+// prisma.user1.create({
+// })
+function insertUser(username, password, firstName, lastName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const res = yield prisma.user1.create({
+                data: {
+                    username,
+                    password,
+                    firstName,
+                    lastName
+                }
+            });
+            console.log(res);
+        }
+        catch (error) {
+            console.error("Error inserting user:", error);
+        }
+    });
+}
+function updateUser(username, { firstName, lastName }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const res = yield prisma.user1.update({
+                where: {
+                    username
+                },
+                data: {
+                    firstName,
+                    lastName
+                }
+            });
+            console.log(res);
+        }
+        catch (error) {
+            console.error("Error updating user:", error);
+        }
+    });
+}
+updateUser('user1', { firstName: 'Anish', lastName: 'Doe' });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,61 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+// prisma.user1.create({
+
+// })
+
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+    try {
+        const res = await prisma.user1.create({
+            data: {
+                username,
+                password,
+                firstName,
+                lastName
+            }
+        });
+        console.log(res);
+    } catch (error) {
+        console.error("Error inserting user:", error);
+    }
+}
+
+// insertUser('user1', 'password1', 'John', 'Doe')
+
+
+
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+
+    try {
+        const res = await prisma.user1.update({
+            where: {
+                username
+            },
+            data: {
+                firstName,
+                lastName
+            }
+        });
+        console.log(res);
+    } catch (error) {
+        console.error("Error updating user:", error);
+    }
+  
+}
+
+//  updateUser('user1', { firstName: 'Anish', lastName: 'Doe' });
+
+
+
